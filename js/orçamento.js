fd851dd6e95e75a9a23499103cf74de829ca2a47
Adriano047 <adrianocardoso047@gmail.com>
2022-11-28 11:07:40 -0300

    melhoramento da modalidade

--- js/orçamento.js
+++ js/orçamento.js
@@ -3,47 +3,56 @@
 let capturando = "";
 let produto = "";
 let total = "";
+let modalidade = "";
 function capturar () {
     produto = document.getElementById('modelo').value;
-    if (produto == 'Produto A') {
-        produto = 20
-    }else if (produto == 'Produto B') {
-        produto = 30
-    }else if (produto == 'Produto C') {
-        produto = 40
-    }else if (produto == 'Produto D') {
-        produto = 50
+    modalidade = document.getElementById('modalidade').value
+
+    if (modalidade == 'Compra') {
+        if (produto == 'computador 01X') {
+            produto = 3057.30
+        }else if (produto == 'computador 02X') {
+            produto = 3341.10
+        }else if (produto == 'computador 03X') {
+            produto = 4811.70
+        }else if (produto == 'computador 04X') {
+            produto = 8689.44
+        }
+    }else if (modalidade == 'Aluguel') {
+        if (produto == 'computador 01X') {
+            produto = 154.80
+        }else if (produto == 'computador 02X') {
+            produto = 175.44
+        }else if (produto == 'computador 03X') {
+            produto = 251.55
+        }else if (produto == 'computador 04X') {
+            produto = 452.79
+        }
     }
     capturando = document.getElementById('Quantidade').value;
         total = produto * capturando
 
-    if (capturando <= 50) {
-        document.getElementById('valorDigitado').innerHTML = total;
-    }else if (capturando > 50 && capturando <= 100 ) {
-        document.getElementById('valorDigitado').innerHTML = total - total / 100 * 3 ;
-    }else if (capturando > 100 && capturando <= 150 ) {
-        document.getElementById('valorDigitado').innerHTML = total - total / 100 * 6 ;
-    }else if (capturando > 150 && capturando <= 200 ) {
-        document.getElementById('valorDigitado').innerHTML = total - total / 100 * 9 ;
-    }else if (capturando > 200 && capturando <= 250 ) {
-        document.getElementById('valorDigitado').innerHTML = total - total / 100 * 12 ;
-    }else if (capturando > 250 && capturando <= 300 ) {
-        document.getElementById('valorDigitado').innerHTML = total - total / 100 * 15 ;
-    }else if (capturando > 300 && capturando <= 350 ) {
-        document.getElementById('valorDigitado').innerHTML = total - total / 100 * 18 ;
-    }else if (capturando > 350 && capturando <= 400 ) {
-        document.getElementById('valorDigitado').innerHTML = total - total / 100 * 21 ;
-    }else if (capturando > 400 ) {
-        document.getElementById('valorDigitado').innerHTML = total - total / 100 * 24 ;
+    if (capturando <= 9) {
+        document.getElementById('valorDigitado').innerHTML = 'R$' + total;
+    }else if (capturando >= 10 && capturando < 20 ) {
+        document.getElementById('valorDigitado').innerHTML = 'R$' + (total - total / 100 * 7) ;
+    }else if (capturando >= 20 && capturando < 30 ) {
+        document.getElementById('valorDigitado').innerHTML = 'R$' + (total - total / 100 * 10) ;
+    }else if (capturando >= 30 && capturando < 50 ) {
+        document.getElementById('valorDigitado').innerHTML = 'R$' + (total - total / 100 * 15) ;
+    }else if (capturando >= 50 && capturando < 100 ) {
+        document.getElementById('valorDigitado').innerHTML = 'R$' + (total - total / 100 * 17) ;
+    }else if (capturando >= 100 && capturando < 500 ) {
+        document.getElementById('valorDigitado').innerHTML = 'R$' + (total - total / 100 * 19) ;
+    }else if (capturando > 500 ) {
+        document.getElementById('valorDigitado').innerHTML = 'R$' + (total - total / 100 * 22) ;
         }
 }
 // impedir o minimo de aluguel
-let quantidade = "";
-let modalidade = "";
+
 function minimo () {
-    quantidade = document.getElementById('Quantidade').value
     modalidade = document.getElementById('modalidade').value
-    if (quantidade < 10 && modalidade == 'Aluguel') {
+    if (capturando < 10 && modalidade == 'Aluguel') {
 
         alert("A quantidade minima para alugar é de 10 unidades")
     }
